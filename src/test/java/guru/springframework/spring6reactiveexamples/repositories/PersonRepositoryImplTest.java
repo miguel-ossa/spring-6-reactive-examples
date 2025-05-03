e9125b17b524e60b987521312d948e6730e5e445
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-03 11:35:38 +0100

    Flux Operations

--- src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -3,7 +3,10 @@ package guru.springframework.spring6reactiveexamples.repositories;
 import guru.springframework.spring6reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 class PersonRepositoryImplTest {
 
@@ -23,19 +26,45 @@ class PersonRepositoryImplTest {
     void testGetByIdSubscriber() {
         Mono<Person> personMono = personRepository.getById(1);
 
-        personMono.subscribe(person -> {
-            System.out.println(person);
-        });
+        personMono.subscribe(System.out::println);
     }
 
     @Test
     void testMapOperation() {
         Mono<Person> personMono = personRepository.getById(1);
 
-        personMono.map(person -> {
-            return person.getFirstName();
-        }).subscribe(firstName -> {
-            System.out.println(firstName);
-        });
+        personMono.map(Person::getFirstName).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person);
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(list -> list.forEach(person -> System.out.println(person.getFirstName())));
     }
 }
